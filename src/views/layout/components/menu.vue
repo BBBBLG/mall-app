d81b63b64292bbc5fa03d6b8825a96e3220fa895
BBBBLG <dingkunpeng1996@gmail.com>
2020-11-26 21:05:55 +0800

    修复一些bug，产生一些新bug

--- src/views/layout/components/menu.vue
+++ src/views/layout/components/menu.vue
@@ -8,19 +8,19 @@
       :inline-collapsed="$store.state.collapsed"
     >
       <template v-for="route in $store.state.menuRoutes">
-        <a-sub-menu v-if="!route.meta.hidden" :key="route.title">
+       <a-sub-menu v-if="!route.meta.hidden" :key="route.name">
           <span slot="title">
-            <a-icon :type="route.meta.icon" />
-            <span>{{ route.meta.title }}</span>
-          </span>
-          <a-menu-item v-for="child in route.children" :key="child.title">
-            <router-link :to="{ name: child.name }">
-              <a-icon :type="child.meta.icon" />
-              {{ child.meta.title }}
-            </router-link>
-          </a-menu-item>
-        </a-sub-menu>
-      </template>
+          <a-icon :type="route.meta.icon" />
+          <span>{{route.meta.title}}</span>
+        </span>
+        <a-menu-item  v-for="child in route.children" :key="child.name">
+          <router-link :to="{name: child.name}">
+            <a-icon :type="child.meta.icon" />
+            {{child.meta.title}}
+          </router-link>
+        </a-menu-item>
+      </a-sub-menu>
+    </template>
     </a-menu>
   </div>
 </template>
